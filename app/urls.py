d23462d65e216d62976021cfded30cd3c68d862d
Pedro Leal Lovatto <pedrolovatto99@gmail.com>
2025-11-12 19:34:46 -0300

    refactor: classe listCreate das views foram alteradas para um modo mais manual

--- app/urls.py
+++ app/urls.py
@@ -5,17 +5,20 @@ from .views import (BlocosListCreate, BlocosRetrieverUpdateDestroy,
 
 
 urlpatterns = [
-    path("blocos/", BlocosListCreate.as_view(), name="listar__criar_blocos"),
+    path("blocos/", BlocosListCreate.as_view(), name="listar_criar_blocos"),
+
     path("blocos/<int:pk>", BlocosRetrieverUpdateDestroy.as_view(),
          name="visualizar_atualizar_deletar_bloco"),
 
     path("apartamentos/", ApartamentosListCreate.as_view(),
-         name="listar__criar_apartamentos"),
+         name="listar_criar_apartamentos"),
+
     path("apartamentos/<int:pk>", ApartamentosRetriverUpdateDestroy.as_view(),
          name="visualizar_atualizar_deletar_apartamento"),
 
     path("residentes/", ResidenteListCreate.as_view(),
-         name="listar__criar_residentes"),
+         name="listar_criar_residentes"),
+
     path("residentes/<int:pk>", ResidenteRetrieverUpdateDestroy.as_view(),
          name="visualizar_atualizar_deletar_residente"),
 
--- app/views.py
+++ app/views.py
@@ -2,12 +2,27 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import Bloco, Apartamento, Residente
 from .serializers import BlocoSerializer, ApartamentoSerializer, ResidenteSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
-
-class BlocosListCreate(generics.ListCreateAPIView):
+class BlocosListCreate(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
     queryset = Bloco.objects.all()
     serializer_class = BlocoSerializer
+
+    def get(self, request, *args, **kwargs):
+
+        blocos = self.queryset.all()
+        serializer = self.serializer_class(blocos, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request, *args, **kwargs):
+
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class BlocosRetrieverUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
@@ -16,10 +31,24 @@ class BlocosRetrieverUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = BlocoSerializer
 
 
-class ApartamentosListCreate(generics.ListCreateAPIView):
+class ApartamentosListCreate(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
     queryset = Apartamento.objects.all()
     serializer_class = ApartamentoSerializer
+
+    def get(self, request, *args, **kwargs):
+
+        apartamentos = self.queryset.all()
+        serializer = self.serializer_class(apartamentos, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request, *args, **kwargs):
+
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ApartamentosRetriverUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
@@ -28,10 +57,24 @@ class ApartamentosRetriverUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ApartamentoSerializer
 
 
-class ResidenteListCreate(generics.ListCreateAPIView):
+class ResidenteListCreate(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
     queryset = Residente.objects.all()
     serializer_class = ResidenteSerializer
+
+    def get(self, request, *args, **kwargs):
+
+        residentes = self.queryset.all()
+        serializer = self.serializer_class(residentes, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request, *args, **kwargs):
+
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ResidenteRetrieverUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
